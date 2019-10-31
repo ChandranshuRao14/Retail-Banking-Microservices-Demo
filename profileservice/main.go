9689dc908810f1664b3797980ef26ef94ab70d79
meganzhao <meganzhao@google.com>
2019-10-30 21:54:33 -0500

    Build a simple microservice with create and read feature through REST API.
    Users can create a user profile and retrieve a user profile by the
    user's ID through API endpoints.

---
+++ profileservice/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"encoding/json"
+	//"os"
+	//"io"
+	"log"
+	//"fmt"
+	"strings"
+	//"path"
+	"github.com/gorilla/mux"
+	//"github.com/gorilla/handlers"
+	"strconv"
+	"google.golang.org/appengine"
+	//"google.golang.org/appengine/datastore"
+	"cloud.google.com/go/datastore"
+)
+
+// Kind in Datastore
+type User struct {
+	UserID int64 `json: "userid"`
+	Username string `json: "username"`
+	AccountBalance float64 `json: "accountbalance"`
+}
+
+var dsClient *datastore.Client
+
+func main() {
+	ctx := context.Background()
+
+	var err error
+	// project ID
+	dsClient, err = datastore.NewClient(ctx, "meganzhao-test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	registerHandlers()
+	appengine.Main()
+}
+
+func registerHandlers() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", createHandler).Methods("POST")
+	r.HandleFunc("/user/{id}", readHandler).Methods("GET")
+	r.HandleFunc("/user/delete/{id}", deleteHandler).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// createHandler adds a user to the database.
+func createhHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	key := datastore.IncompleteKey("User", nil)
+	if _, err := dsClient.Put(ctx, key, &user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func readHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/user/"))
+	if err != nil {
+		// change error to invalid ID (should be an int)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user := make([]*User, 0)
+	q := datastore.NewQuery("User").Filter("UserID =", id)
+	// have to use a slice to save the result? or have to use getall?
+	if _, err := dsClient.GetAll(ctx, q, &user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res, _ := json.Marshal(&user)
+	w.Write(res)
+}
+
+// func deleteHandler(w http.ResponseWriter, r *http.Request) {
+// 	ctx := appengine.NewContext(r)
+// 	id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/user/delete/"))
+// 	if err != nil {
+// 		// change error to invalid ID (should be an int)
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// }
+
+
+// 	// Respond to App Engine and Compute Engine health checks.
+// 	// Indicate the server is healthy.
+// 	// r.Methods("GET").Path("/_ah/health").HandlerFunc(
+// 	// 	func(w http.ResponseWriter, r *http.Request) {
+// 	// 		w.Write([]byte("ok"))
+// 	// 	})
+		
+// 	// Delegate all of the HTTP routing and serving to the gorilla/mux router.
+// 	// Log all requests using the standard Apache format.
+// 	// http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, r))
+// }
+
+
+
+
+
+
+
+
+
+
+
